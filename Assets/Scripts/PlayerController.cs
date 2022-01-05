caecb26c26fae335314e3a0f1ea6cf8c0d91d1c5
Jon Whisman <jon@pixelgraft.com>
2022-01-04 20:42:45 -0800

    PlayerController setup so that camera / player rotate based on mouse movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     public CharacterController charCon;
 
     private Vector3 moveInput;
+    public Transform camTrans;
+
+    public float mouseSensitivity;
+
+    public bool invertX;
+    public bool invertY;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +26,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get Keyboard / Game Controller input
-        moveInput.x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        ///////////*********** Get Keyboard / Game Controller input ********** ///////////
+
+        //moveInput.x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 vertMove = transform.forward * Input.GetAxisRaw("Vertical");
+        Vector3 horiMove = transform.forward * Input.GetAxisRaw("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed;
 
         // Move the player based on the moveInput Vector3
-        charCon.Move(moveInput);
+        charCon.Move(moveInput * Time.deltaTime);
+
+        ///////////*********** Get Mouse Movement / Control Camera Rotation ********** ///////////
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        //Check Invert X or Y axis flips are
+        if (invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
+
+
+        // Apply Y axis rotation (side to side) to player based on Mouse Input
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+        // Apply X axis rotation (up and down) to camera rotation based on mouse input. The -mouseinput is so that when we move our mouse up, the camera rotation on the X axis is applied correctly and not inverted
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
