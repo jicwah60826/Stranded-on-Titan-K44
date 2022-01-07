53e0cdb029d907dc23d4de337e774358b2b87130
Jon Whisman <jon@pixelgraft.com>
2022-01-07 08:52:46 -0800

    Enemy now chases and loses player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,18 +8,44 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody theRB;
 
+    private bool chasing;
+    public float distanceToChase = 10f, distanceToLose = 15f;
+
+    private Vector3 targetPoint;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(PlayerController.instance.transform.position); //look at the transform position of the player controller instance
+        // enemy will look around on Y axis for player, but will look up or down. Ensures enemy does not float up into air if player jumps
+        targetPoint = PlayerController.instance.transform.position;
+        targetPoint.y = transform.position.y;
 
-        theRB.velocity = transform.forward * moveSpeed; //move the enemy forward by the moveSpeed
+        if (!chasing)
+        {
+            // is enemy position less than diatance to chase? If so, chase!
+            if (Vector3.Distance(transform.position, targetPoint) < distanceToChase)
+            {
+                chasing = true;
+            }
+        }
+        else
+        {
+            transform.LookAt(targetPoint); //look at the transform position of the player controller instance
+            theRB.velocity = transform.forward * moveSpeed; //move the enemy forward by the moveSpeed
+
+            // stop chasing player if outside range of distance to lose
+            if (Vector3.Distance(transform.position, targetPoint) > distanceToLose)
+            {
+                chasing = false;
+            }
+
+        }
     }
 }
