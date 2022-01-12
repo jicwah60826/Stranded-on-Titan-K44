5bd8ad6b51dbd63c5ce296dfbbaa4703b9ed3d94
Jon Whisman <jon@pixelgraft.com>
2022-01-11 16:54:42 -0800

    re-spawn checkpoints and trigger zones setup

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckPointController : MonoBehaviour
+{
+
+    public string cpName;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "_cp"))
+        {
+            if (PlayerPrefs.GetString(SceneManager.GetActiveScene().name + "_cp") == cpName)
+            {
+                // do stuff
+                PlayerController.instance.transform.position = transform.position;
+                Debug.Log("Player starting at" + cpName);
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", ""); // clear out stored value for cpname
+            Debug.Log("cpName cleared from playerprefs");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", cpName);
+            Debug.Log("Player hit" + cpName);
+        }
+    }
+}
