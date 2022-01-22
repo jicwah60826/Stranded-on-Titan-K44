535842339684e6255608e33a4644ac59b5a5dd9a
Jon Whisman <jon@pixelgraft.com>
2022-01-22 15:19:23 -0800

    Fixed walking sounds, but still need to stop them when player has jumped

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,7 +7,7 @@ public class AudioManager : MonoBehaviour
 
     public static AudioManager instance;
 
-    public AudioSource bgMusic;
+    public AudioSource bgMusic, bgAmbience;
 
     public AudioSource[] soundEffects;
 
