f1932e7d08e42bb8a1d5cca2e42d91d1e3e95a3b
Jon Whisman <jon@pixelgraft.com>
2022-01-23 22:24:11 -0800

    Adjusted enemy and player speed (walk & run). Added new Ambient music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,18 +15,6 @@ public class AudioManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void StopBGM()
